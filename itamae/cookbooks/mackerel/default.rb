13309829a9a50ebfc24a5e0cc83a2a0f45960e04
fukuiretu <s0232101@gmail.com>
2015-07-29 07:14:04 +0900

    Modify mackerel config

--- itamae/cookbooks/mackerel/default.rb
+++ itamae/cookbooks/mackerel/default.rb
@@ -1,20 +1,15 @@
+MACKEREL_CONFIG = "/etc/mackerel-agent/mackerel-agent.conf"
+
 execute 'Installing the official repository file' do
   command "curl -fsSL https://mackerel.io/assets/files/scripts/setup-yum.sh | sh"
   not_if "yum list | grep mackerel-agent"
 end
 
 package "mackerel-agent"
-
-service "mackerel-agent" do
-  user 'root'
-  action :restart
-end
+service "mackerel-agent"
 
 execute 'Setting mackerel config' do
-  user 'root'
-  command <<-EOS
-  echo apikey = "#{Global.secret.mackerel.apikey}" >> /etc/mackerel-agent/mackerel-agent.conf
-  EOS
-  not_if "test -e /etc/mackerel-agent/mackerel-agent.conf"
+  command "echo 'apikey = \"#{Global.secret.mackerel.apikey}\"' >> #{MACKEREL_CONFIG}"
+  not_if "grep 'apikey = \"#{Global.secret.mackerel.apikey}\"' #{MACKEREL_CONFIG}"
   notifies :restart, "service[mackerel-agent]", :delay
 end
